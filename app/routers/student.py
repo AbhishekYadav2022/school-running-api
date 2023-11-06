a18e3b8127f826fd26c4e3db5c938c0c93ef4d4c
Abhishek Kumar Yadav <yadavabhishek5869@gmail.com>
2023-11-07 05:00:15 +0530

    query params added

--- app/routers/student.py
+++ app/routers/student.py
@@ -3,6 +3,7 @@ from .. import models, schemas
 from typing import List
 from ..database import engine, get_db
 from sqlalchemy.orm import Session
+from typing import Optional
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,43 +25,80 @@ def create_student(student: schemas.CreateStudent, db: Session = Depends(get_db)
 
 # Get All Students
 @router.get("/", response_model=List[schemas.GetStudent])
-def get_all_students(db: Session = Depends(get_db)):
-    students = db.query(models.Student).all()
+def get_all_students(
+    db: Session = Depends(get_db),
+    skip: int = 0,
+    name: Optional[str] = "",
+    std_class: Optional[str] = "",
+    phone: Optional[str] = "",
+    limit: int = 20,
+):
+    students = (
+        db.query(models.Student)
+        .filter(models.Student.name.contains(name))
+        .filter(models.Student.std_class.contains(std_class))
+        .filter(models.Student.phone.contains(phone))
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
     return students
 
-# Get student by id 
-@router.get("/{id}", )
-def get_student_by_id(id: int, db: Session = Depends(get_db)):
+
+# Get student by id
+@router.get(
+    "/{id}",
+)
+def get_student_by_id(
+    id: int,
+    db: Session = Depends(get_db),
+):
     student = db.query(models.Student).filter(models.Student.id == id).first()
 
-    # If user not found 
+    # If user not found
     if not student:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with id {id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Student with id {id} not found",
+        )
     return student
 
-# Delete student 
+
+# Delete student
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_student(id: int, db:Session=Depends(get_db)):
-    student = db.query(models.Student).filter(models.Student.id == id)
+def delete_student(
+    id: int,
+    db: Session = Depends(get_db),
+):
+    student = db.query(models.Student).filter(
+        models.Student.id == id,
+    )
 
     # Checking if the student exists
     if student.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No student found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No student found",
+        )
     student.delete(synchronize_session=False)
     db.commit()
 
-# Update student 
+
+# Update student
 @router.put("/{id}", response_model=schemas.GetStudent)
-def update_student(id: int, updated_student: schemas.CreateStudent, db:Session = Depends(get_db)):
+def update_student(
+    id: int, updated_student: schemas.CreateStudent, db: Session = Depends(get_db)
+):
     std_query = db.query(models.Student).filter(models.Student.id == id)
 
     student = std_query.first()
 
     # Raise Exception
     if student == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No student found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No student found",
+        )
     std_query.update(updated_student.dict(), synchronize_session=False)
     db.commit()
     return std_query.first()
-
-

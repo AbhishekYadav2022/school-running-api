c069c7b9ede7f9bbf42737fbe7df4055eb3e789b
Abhishek Kumar Yadav <yadavabhishek5869@gmail.com>
2024-01-24 08:26:16 +0530

    cors added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers.employees import (
     employee,
     department,
@@ -33,12 +35,18 @@ model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():
-    return {
-        "message":"👋Hello, How are you?"
-    }
+    return {"message": "👋Hello, How are you?"}
 
 
 app.include_router(employee.router)
